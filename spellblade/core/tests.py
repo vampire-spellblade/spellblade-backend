8499c569dec774edef7e7960892cb4783b88ae89
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2024-12-14 14:48:15 -0600

    Setup - rest_framework & corsheaders

--- spellblade/core/tests.py
+++ spellblade/core/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
-
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
+from . import models
+from . import serializers
