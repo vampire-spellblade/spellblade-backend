4de9e8626c41d3ac54e2b50c07036da31929a1c9
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2024-12-15 15:30:27 -0600

    Updated views and tests to rely on messages instead

--- spellblade/core/tests.py
+++ spellblade/core/tests.py
@@ -2,6 +2,7 @@ from rest_framework.test import APITestCase
 from rest_framework.reverse import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from django.utils.translation import gettext_lazy as _
 from . import models
 from . import serializers
 
@@ -12,79 +13,83 @@ class TestLogin(APITestCase):
             'first_name': 'John',
             'last_name': 'Doe',
             'email': 'jdoe@example.com',
-            'password': 'y79rNX#+l45M',
+            'password': 'ABC123!xyz',
         }
 
         serializer = serializers.UserSerializer(data=data)
 
         if serializer.is_valid():
             self.user = serializer.save()
-        else:
-            print(serializer.errors)
 
         self.url = reverse('login')
 
-    def test_success(self):
+    def test_login_success(self):
         data = {
             'email': 'jdoe@example.com',
-            'password': 'y79rNX#+l45M',
+            'password': 'ABC123!xyz',
         }
 
         response = self.client.post(self.url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['email'], self.user.email)
-        self.assertEqual(response.data['first_name'], self.user.first_name)
-        self.assertEqual(response.data['last_name'], self.user.last_name)
-        self.assertTrue('token' in response.data)
+        self.assertEqual(response.data['message'], _('Login successful'))
+        self.assertEqual(response.data['user']['email'], self.user.email)
+        self.assertEqual(response.data['user']['first_name'], self.user.first_name)
+        self.assertEqual(response.data['user']['last_name'], self.user.last_name)
+        self.assertTrue('token' in response.data['user'])
 
-    def test_missing_email(self):
+    def test_login_missing_email(self):
         data = {
-            'password': 'y79rNX#+l45M',
+            'password': 'ABC123!xyz',
         }
 
         response = self.client.post(self.url, data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['error'], _('Invalid email or password'))
 
-    def test_missing_password(self):
+    def test_login_missing_password(self):
         data = {
             'email': 'jdoe@example.com',
         }
 
         response = self.client.post(self.url, data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['error'], _('Invalid email or password'))
 
-    def test_invalid_email_format(self):
+    def test_login_invalid_email_format(self):
         data = {
             'email': 'jdoe',
-            'password': 'y79rNX#+l45M',
+            'password': 'ABC123!xyz',
         }
 
         response = self.client.post(self.url, data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['error'], _('Invalid email or password'))
 
-    def test_wrong_email(self):
+    def test_login_wrong_email(self):
         data = {
             'email': 'jdoe7@example.com',
-            'password': 'y79rNX#+l45M',
+            'password': 'ABC123!xyz',
         }
 
         response = self.client.post(self.url, data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['error'], _('Invalid email or password'))
 
-    def test_wrong_password(self):
+    def test_login_wrong_password(self):
         data = {
             'email': 'jdoe@example.com',
-            'password': 'y79rNX#+l45m',
+            'password': 'abc123!XYZ',
         }
 
         response = self.client.post(self.url, data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['error'], _('Invalid email or password'))
 
 class TestLogout(APITestCase):
 
@@ -93,15 +98,13 @@ class TestLogout(APITestCase):
             'first_name': 'John',
             'last_name': 'Doe',
             'email': 'jdoe@example.com',
-            'password': 'y79rNX#+l45M',
+            'password': 'ABC123!xyz',
         }
 
         serializer = serializers.UserSerializer(data=data)
 
         if serializer.is_valid():
             self.user = serializer.save()
-        else:
-            print(serializer.errors)
 
         self.url = reverse('logout')
 
@@ -110,24 +113,76 @@ class TestLogout(APITestCase):
             'password': data['password'],
         }, format='json')
 
-        self.token = response.data['token']
+        self.token = response.data['user']['token']
 
-    def test_success(self):
+    def test_logout_success(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
 
         response = self.client.post(self.url, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], _('Logout successful'))
         self.assertFalse(Token.objects.filter(user=self.user).exists())
 
-    def test_unauthenticated(self):
+    def test_logout_unauthenticated(self):
         response = self.client.post(self.url)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_invalid_token(self):
+    def test_logout_invalid_token(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token invalidtoken123')
 
         response = self.client.post(self.url)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class TestSignup(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('signup')
+
+    def test_signup_successful_signup(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'jdoe@example.com',
+            'password': 'ABC123!xyz',
+        }
+
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], _('Signup successful'))
+        user = models.User.objects.get(email=data['email'])
+        self.assertEqual(user.first_name, data['first_name'])
+        self.assertEqual(user.last_name, data['last_name'])
+        self.assertTrue('token' in response.data['user'])
+
+    def test_signup_account_already_exists(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'jdoe@example.com',
+            'password': 'ABC123!xyz',
+        }
+
+        serializer = serializers.UserSerializer(data=data)
+
+        if serializer.is_valid():
+            self.user = serializer.save()
+
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['error'], _('Account already exists'))
+
+    def test_signup_invalid_signup_data(self):
+        data = {
+            'email': 'invalid@example.com',
+            'password': 'short',
+        }
+
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['error'], _('Invalid data'))
--- spellblade/core/views.py
+++ spellblade/core/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
 from . import models
 from . import serializers
 
@@ -19,29 +20,51 @@ def login(request):
         token, created = Token.objects.get_or_create(user=user)
 
         return Response({
-            'email': user.email,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'token': token.key,
+            'message': _('Login successful'),
+            'user': {
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'token': token.key,
+            },
         }, status=status.HTTP_200_OK)
     else:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return Response({ 'error': _('Invalid email or password') }, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def logout(request):
-    if request.user.is_authenticated:
-        try:
-            token = Token.objects.get(user=request.user)
-            token.delete()
+    try:
+        token = Token.objects.get(user=request.user)
+        token.delete()
 
-            return Response(status=status.HTTP_200_OK)
-        except Token.DoesNotExist:
-            pass
-    else:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return Response({ 'message': _('Logout successful') }, status=status.HTTP_200_OK)
+    except Token.DoesNotExist:
+        pass
 
 @api_view(['POST'])
 def signup(request):
-    pass
+    email = request.data.get('email')
+
+    if models.User.objects.filter(email=email).exists():
+        return Response({ 'error': _('Account already exists') }, status=status.HTTP_200_OK)
+
+    serializer = serializers.UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        user = serializer.save()
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'message': _('Signup successful'),
+            'user': {
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'token': token.key,
+            },
+        }, status=status.HTTP_200_OK)
+    else:
+        return Response({ 'error': _('Invalid data') }, status=status.HTTP_200_OK)
