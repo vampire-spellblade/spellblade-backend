d34ad501a54e9d5b595119a4c02df2ec8556e6bd
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2024-12-21 12:49:24 -0600

    Removed everything

--- spellblade/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('login/renew/', views.login_renew, name='login_renew'),
-    path('signup/', views.signup, name='signup'),
-]
