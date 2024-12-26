9aa741a4f5a20366ade75373a07566564028003f
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2024-12-25 21:27:09 -0600

    New Knowledge Unlocked: In DEBUG mode, ALLOWED_HOSTS, if empty, is instead set to ['.localhost', '127.0.0.1', '[::1]']

--- spellblade/settings.py
+++ spellblade/settings.py
@@ -9,7 +9,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[]) + ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
