98194b057e4058b0f7a149847582cc05ad65d5f6
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2024-12-27 17:48:48 -0600

    Added a new validator and removed two default validators

--- spellblade/settings.py
+++ spellblade/settings.py
@@ -102,8 +102,7 @@ AUTH_USER_MODEL = 'account.User'
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {'NAME': 'account.validators.PasswordComplexityValidator',},
 ]
 
 SERVER_EMAIL = env('SERVER_EMAIL').strip()
