f81738b4af7fd7ffbb2f240372585c9db9dea049
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-01 15:56:14 -0600

    Minor change

--- spellblade/settings.py
+++ spellblade/settings.py
@@ -44,12 +44,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 10,
-        }
-    },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 10,},},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
 
