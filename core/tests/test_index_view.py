fb3c9ccc3d2cbf569864b47e80ae89df0e3708ee
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-02 08:06:51 -0600

    Added seperate folder for views and tests in core app

--- core/tests/test_index_view.py
+++ core/tests/test_index_view.py
@@ -1,12 +1,15 @@
 from rest_framework.test import APITestCase
 from rest_framework.reverse import reverse
 from rest_framework import status
-from . import serializers
+from .. import serializers
 
 class TestIndexView(APITestCase):
 
+    def setUp(self):
+        self.index = reverse('index')
+
     def test_index_view_success(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(self.index)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['message'], 'Hello, World!')
--- core/views/index_view.py
+++ core/views/index_view.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from . import serializers
+from .. import serializers
 
 class IndexView(APIView):
 
