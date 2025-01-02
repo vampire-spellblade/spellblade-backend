b016671fc13a248a763007a795a6cbaa24c8cd84
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-02 08:32:31 -0600

    Updated ident app and configured url in spellblade

--- spellblade/urls.py
+++ spellblade/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
+    path('ident/', include('ident.urls')),
 ]
 
 if settings.DEBUG:
