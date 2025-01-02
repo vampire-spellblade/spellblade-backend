fe1da1c65f54423307636347e5949b9c662c9433
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-02 12:25:48 -0600

    Renamed app

--- account/apps.py
+++ account/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class IdentConfig(AppConfig): # pylint: disable=missing-class-docstring
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ident'
+    name = 'account'
     verbose_name = 'Authentication & Authorization'
--- spellblade/settings.py
+++ spellblade/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
-    'ident',
+    'account',
     'core',
 ]
 
