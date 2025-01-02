718e3ff602e42c96d280354922b8a11f9d769d08
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-01 21:47:02 -0600

    App setup and a basic view to test functionality

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
+from . import serializers
 
-# Create your tests here.
+class TestIndexView(APITestCase):
+
+    def test_index_view_success(self):
+        response = self.client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], 'Hello, World!')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
-# Create your views here.
+class IndexView(APIView):
+
+    def get(self, request):
+        return Response({'message': 'Hello, World!'}, status=status.HTTP_200_OK)
