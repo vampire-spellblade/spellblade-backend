23b4ee4b2f23f626e4115293982f25f8d023786b
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-03 11:27:57 -0600

    .env file doesn't work properly

--- spellblade/settings.py
+++ spellblade/settings.py
@@ -6,7 +6,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-env.read_env(BASE_DIR / '.env')
+environ.Env.read_env(BASE_DIR / '.env')
 
 SECRET_KEY = env('SECRET_KEY').strip()
 SECRET_KEY_FALLBACKS = \
