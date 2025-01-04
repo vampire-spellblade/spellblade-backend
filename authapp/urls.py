c1e4a66e862b60582f4ab252d7f36192774fb350
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-04 10:25:39 -0600

    Combined signup field requirements and signup views into one and identified an issue where browsable API doesn't reflect password requirements and errors properly

--- authapp/urls.py
+++ authapp/urls.py
@@ -4,12 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenBlacklistView,
 )
-from .views.password_requirements import PasswordRequirementsView
 from .views.signup import SignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('signup/password-requirements/', PasswordRequirementsView.as_view(), name='signup_password_requirements'),
 
     # TODO: Remove and replace with custom views. (Priority: High)
     path('login/', TokenObtainPairView.as_view(), name='login'),
--- authapp/views/signup.py
+++ authapp/views/signup.py
@@ -1,8 +1,11 @@
+from importlib import import_module
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from spellblade.settings import AUTH_PASSWORD_VALIDATORS
 from ..serializers.create_user import UserCreationSerializer
+from ..models import User
 
 class SignUpView(APIView):
     '''Registers a new user.'''
@@ -11,6 +14,26 @@ class SignUpView(APIView):
 
     def get_serializer(self):
         return self.serializer_class()
+
+    def get(self, request):
+        username_field = User._meta.get_field(User.USERNAME_FIELD)
+        response = {
+            'username': [username_field.help_text],
+            'password': [],
+        }
+
+        for validator in AUTH_PASSWORD_VALIDATORS:
+            try:
+                validator_class_path = validator['NAME']
+                module_path, class_name = validator_class_path.rsplit('.', 1)
+                module = import_module(module_path)
+                validator_class = getattr(module, class_name)
+
+                response['password'].append(validator_class().get_help_text())
+            except:
+                pass
+
+        return Response(response, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
