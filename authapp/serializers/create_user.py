1daa1165905e656cefb5232545db14d23ee207fb
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-04 10:46:16 -0600

    Fixed the previous problem but not perfectly

--- authapp/serializers/create_user.py
+++ authapp/serializers/create_user.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
-from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 
 User = get_user_model()
@@ -12,7 +11,13 @@ class UserCreationSerializer(serializers.ModelSerializer):
         fields = ('email', 'username', 'password',)
 
     def validate(self, attrs):
-        validate_password(attrs['password'], user=User(**attrs))
+        try:
+            validate_password(attrs['password'], user=User(**attrs))
+        except Exception as e:
+            # Using ValidationError here doesn't seem to work.
+            # More specifically, the error is not caught if ValidationError is in use despite
+            # the docs saying that that's what validate_password throws.
+            raise serializers.ValidationError({'password': e.messages})
         return attrs
 
     def create(self, validated_data):
