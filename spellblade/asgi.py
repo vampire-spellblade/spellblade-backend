5d50bd0b89844ffbbcaf1ad38a9e1409a977c0e9
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-11 22:42:52 -0600

    Added spellblade project

---
+++ spellblade/asgi.py
@@ -0,0 +1,6 @@
+"""ASGI config for spellblade project. It exposes the ASGI callable as a module-level variable named application."""
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spellblade.settings')
+application = get_asgi_application()
