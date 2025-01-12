ac135ac0e8a832b4414c2f34cc9c1ad361d80b88
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-11 20:42:40 -0600

    Created a simple app to test gunicorn

---
+++ testapp/views.py
@@ -0,0 +1,9 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class IndexView(APIView):
+
+    def get(self, request):
+        return Response({'message': _('Hello, World!')}, status=status.HTTP_200_OK)
