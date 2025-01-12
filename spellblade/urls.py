3ff7e989c68b135e2c2e4a5bccbcc4e5a0ccac64
Sanjangeet Singh <sanjangeetsingh65@gmail.com>
2025-01-12 08:32:26 -0600

    Renamed core app to spellblade_core

--- spellblade/urls.py
+++ spellblade/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', include('spellblade_core.urls')),
 
     prefix_default_language=True
 )
--- spellblade_core/urls.py
+++ spellblade_core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import IndexView
+from .views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
